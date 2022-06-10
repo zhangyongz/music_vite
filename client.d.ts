8a6b7bc79e8d1f76ca238c8e6d9a30438eef79f9
zhangyong <zhangyong_web@163.com>
2022-06-10 11:03:24 +0800

    feat: 修改i18n插件

---
+++ client.d.ts
@@ -0,0 +1,4 @@
+declare module 'vite-plugin-react-i18n/messages' {
+  const messages: any
+  export default messages
+}
--- plugins/vite-plugin-react-i18n.ts
+++ plugins/vite-plugin-react-i18n.ts
@@ -1,54 +1,47 @@
 import type { Plugin } from 'vite'
 import * as path from 'path'
 import * as fs from 'fs'
-import { useState } from 'react'
 
 const localPath = path.resolve(process.cwd(), './src/locales')
-const i18n = {}
 
-function readDir () {
-  fs.readdir(localPath, function (err, files) {
-    if (err) throw err
-    console.log(files)
-    files.forEach((item) => {
-      readFile(item)
-    })
-  })
+function getRaw (item: string) {
+  const filePath = path.resolve(localPath, item)
+  return fs.readFileSync(filePath, { encoding: 'utf-8' })
 }
 
-function readFile (item: string) {
-  const filePath = path.resolve(localPath, item)
-  const fileName = item.split('.')[0]
-  fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
-    if (err) throw err
-    i18n[fileName] = JSON.parse(data)
+const BUNDLE_IMPORT_ID = 'vite-plugin-react-i18n/messages'
+
+function gennerateBundleResources () {
+  const codes = []
+  const files = fs.readdirSync(localPath)
+  files.forEach((item) => {
+    const { name } = path.parse(item)
+    const source = getRaw(item)
+    codes.push(`${JSON.stringify(name)}:${source}`)
   })
+
+  return `export default {
+    ${codes.join(',\n')}
+  }`
 }
 
 function reactI18n (): Plugin {
-  readDir()
-  // const url = path.resolve(process.cwd(), './src/locales/cn.json')
-  // let en = {}
-  // fs.readFile(url, { encoding: 'utf-8' }, (err, data) => {
-  //   if (err) {
-  //     throw (err)
-  //   }
-  //   en = JSON.parse(data)
-  //   console.log(en)
-  // })
-
   return {
-    name: 'vite-plugin-react-i18n'
+    name: 'vite-plugin-react-i18n',
+
+    resolveId (id: string) {
+      if (id === BUNDLE_IMPORT_ID) {
+        return id
+      }
+    },
+
+    load (id: string) {
+      if (id === BUNDLE_IMPORT_ID) {
+        const codes = gennerateBundleResources()
+        return Promise.resolve(codes)
+      }
+    }
   }
 }
 
-function useI18n () {
-  const [locale, setLocale] = useState('en')
-  const t = (msg: string) => {
-    return i18n[locale][msg]
-  }
-
-  return { locale, setLocale, t }
-}
-
-export { reactI18n, useI18n }
+export { reactI18n }
